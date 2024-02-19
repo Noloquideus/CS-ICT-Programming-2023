088679492079c8adc726f38357dd08183e0f3443
Noloquideus <daniilmanukian@gmail.com>
2024-02-20 02:59:42 +0300

    feat: added is_prime method

--- src/lab2/rsa.py
+++ src/lab2/rsa.py
@@ -9,6 +9,17 @@ class RSA(Cipher):
     def __init__(self):
         super().__init__()
 
+    @staticmethod
+    def is_prime(num, test_iterations=10):
+        if num < 2:
+            return False
+
+        for _ in range(test_iterations):
+            a = random.randint(2, num - 2)
+            if pow(a, num - 1, num) != 1:
+                return False
+        return True
+
     def decrypt(self, text):
         pass
 
