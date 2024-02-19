0111c1aabe1e441f0d64951b22c0549a4055104c
Noloquideus <daniilmanukian@gmail.com>
2024-02-20 02:10:09 +0300

    feat: created template rsa class

---
+++ src/lab2/rsa.py
@@ -0,0 +1,25 @@
+from src.lab2.Cipher import Cipher
+
+
+class RSA(Cipher):
+
+    def __init__(self):
+        super().__init__()
+
+    def generate_key_pair(self):
+        pass
+
+    def generate_prime_number(self):
+        pass
+
+    def generate_public_key(self):
+        pass
+
+    def generate_private_key(self):
+        pass
+
+    def decrypt(self, text):
+        pass
+
+    def encrypt(self, text):
+        pass
