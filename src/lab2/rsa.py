0f1d3c3895be6df51937fe8f8c9a61f2e651134c
Noloquideus <daniilmanukian@gmail.com>
2024-02-20 02:16:30 +0300

    fix: full rollback

--- src/lab2/rsa.py
+++ src/lab2/rsa.py
@@ -1,15 +1,13 @@
 import random
 import math
+
 from src.lab2.Cipher import Cipher
 
 
 class RSA(Cipher):
 
-    def __init__(self, key_size=1024):
+    def __init__(self):
         super().__init__()
-        self.key_size = key_size
-        self.public_key, self.private_key = self.generate_key_pair()
-
 
     def decrypt(self, text):
         pass
